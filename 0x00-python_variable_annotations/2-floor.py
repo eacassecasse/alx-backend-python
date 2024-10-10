7a9a424551b89c699902d58927952826676f538e
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-10-10 21:30:05 +0000

    fix: Correcting shebang to use the most suitable python3 on the user machine

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ This module defines a function floor. """
 
 import math
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ This module defines a function sum_list. """
 
 import typing
